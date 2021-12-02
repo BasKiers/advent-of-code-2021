d334b133016e7638af3cb29264bdb401ba6e68c5
Bas Kiers <kiers.bas@gmail.com>
2021-12-02 10:27:48 +0100

    Refactor classes into vessels

--- src/main/kotlin/days/Day2.kt
+++ src/main/kotlin/days/Day2.kt
@@ -6,36 +6,42 @@ class Day2 : Day(2) {
         Pair(command, amount.toInt())
     }
 
-    open class Position(val x: Int,val y: Int) {
-        open operator fun plus(i: Int) = Position(x, y + i)
-        open operator fun minus(i: Int) = Position(x, y - i)
-        open operator fun times(i: Int) = Position(x + i, y)
+    abstract class Vessel(val x: Int, val y: Int) {
+        abstract fun up(i: Int): Vessel
+        abstract fun down(i: Int): Vessel
+        abstract fun forward(i: Int): Vessel
 
         fun getLocation() = x * y
     }
 
-    class AimedPosition(x: Int,y: Int, val aim: Int = 0): Position(x, y) {
-        override operator fun plus(i: Int) = AimedPosition(x, y, aim + i)
-        override operator fun minus(i: Int) = AimedPosition(x, y, aim - i)
-        override operator fun times(i: Int) = AimedPosition(x + i, y + aim * i, aim)
+    class DiveBoat(x: Int, y: Int): Vessel(x, y) {
+        override fun up(i: Int) = DiveBoat(x, y - i)
+        override fun down(i: Int) = DiveBoat(x, y + i)
+        override fun forward(i: Int) = DiveBoat(x + i, y)
     }
 
-    fun commandRunner(initialPosition: Position, command: Pair<String, Int>): Position {
+    class Submarine(x: Int, y: Int, val aim: Int = 0): Vessel(x, y) {
+        override fun up(i: Int) = Submarine(x, y, aim - i)
+        override fun down(i: Int) = Submarine(x, y, aim + i)
+        override fun forward(i: Int) = Submarine(x + i, y + aim * i, aim)
+    }
+
+    fun executeCommand(vessel: Vessel, command: Pair<String, Int>): Vessel {
         val (direction, amount) = command
-        
+
         return when (direction) {
-            "up" -> initialPosition - amount
-            "down" -> initialPosition + amount
-            "forward" -> initialPosition * amount
-            else -> initialPosition
+            "up" -> vessel.up(amount)
+            "down" -> vessel.down(amount)
+            "forward" -> vessel.forward(amount)
+            else -> vessel
         }
     }
 
     override fun partOne(): Any {
-        return commands.fold(Position(0, 0), ::commandRunner).getLocation()
+        return commands.fold(DiveBoat(0, 0), ::executeCommand).getLocation()
     }
 
     override fun partTwo(): Any {
-        return commands.fold(AimedPosition(0, 0), ::commandRunner).getLocation()
+        return commands.fold(Submarine(0, 0), ::executeCommand).getLocation()
     }
 }

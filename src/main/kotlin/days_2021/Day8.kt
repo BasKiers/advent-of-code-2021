ff92a105d8a37b096f90ae446ba58fe46942a332
Bas Kiers <kiers.bas@gmail.com>
2021-12-08 12:01:58 +0100

    cleaned up a bit now that I know every number is in example

--- src/main/kotlin/days_2021/Day8.kt
+++ src/main/kotlin/days_2021/Day8.kt
@@ -5,7 +5,7 @@ import kotlin.math.pow
 
 class Day8 : Day(8) {
     val input = inputList.map { it.split(" | ") }.map { (examples, answer) ->
-        examples.split(" ").map(String::toCharArray) to answer.split(" ").map(String::toCharArray)
+        examples.split(" ").map(String::toCharArray).map(CharArray::toSet) to answer.split(" ").map(String::toCharArray).map(CharArray::toSet)
     }
 
     class SevenSegmentDisplay(mapping: Map<Int, Set<Char>>, words: List<Set<Char>>) {
@@ -19,76 +19,53 @@ class Day8 : Day(8) {
 
 
         companion object {
-            fun from(examples: List<CharArray>, words: List<CharArray>): SevenSegmentDisplay {
+            fun from(examples: List<Set<Char>>, words: List<Set<Char>>): SevenSegmentDisplay {
                 val numberToSegmentMapping: MutableMap<Int, Set<Char>> = mutableMapOf();
                 for (number in listOf(2, 3, 4, 7, 6, 5)) {
-                    for (example in (examples + words).filter { it.size == number }.map(CharArray::toSet)) {
+                    for (example in examples.filter { it.size == number }) {
                         when (example.size) {
                             2 -> numberToSegmentMapping[1] = example
                             3 -> numberToSegmentMapping[7] = example
                             4 -> numberToSegmentMapping[4] = example
                             7 -> numberToSegmentMapping[8] = example
                             6 -> { // 6 9 0
-                                val overlap = (numberToSegmentMapping.getOrDefault(
-                                    1,
-                                    setOf()
-                                ) + numberToSegmentMapping.getOrDefault(
-                                    7,
-                                    setOf()
-                                )).all { char -> example.contains(char) }
-                                val overlaps4 = numberToSegmentMapping.getOrDefault(
-                                    4,
-                                    setOf()
-                                ).all { char -> example.contains(char) }
+                                val overlaps7 = numberToSegmentMapping[7]!!.all { char -> example.contains(char) }
+                                val overlaps4 = numberToSegmentMapping[4]!!.all { char -> example.contains(char) }
 
-                                if (overlap && overlaps4) {
+                                if (overlaps7 && overlaps4) {
                                     numberToSegmentMapping[9] = example
-                                } else if (overlap) {
+                                } else if (overlaps7) {
                                     numberToSegmentMapping[0] = example
                                 } else {
                                     numberToSegmentMapping[6] = example
                                 }
                             }
                             5 -> { // 2, 5, 3
-                                val overlapWith3 = numberToSegmentMapping.getOrDefault(
-                                    1,
-                                    setOf()
-                                ) + numberToSegmentMapping.getOrDefault(7, setOf())
+                                val overlaps7 = numberToSegmentMapping[7]!!.all { char -> example.contains(char) }
+                                val rightTop = numberToSegmentMapping[1]!!.filter { char ->
+                                    !numberToSegmentMapping[6]!!.contains(char)
+                                }
+                                val overlapsRightTop = rightTop.all { char -> example.contains(char) }
 
-                                if (overlapWith3.all { char -> example.contains(char) }) {
+                                if (overlaps7) {
                                     numberToSegmentMapping[3] = example
-                                } else if (numberToSegmentMapping.contains(4) && numberToSegmentMapping.contains(7)) {
-                                    val mask = numberToSegmentMapping.getOrDefault(
-                                        4,
-                                        setOf()
-                                    ) + numberToSegmentMapping.getOrDefault(7, setOf())
-                                    val res = example.filter { char -> !mask.contains(char) }
-                                    if (res.size > 1) {
-                                        numberToSegmentMapping[2] = example
-                                    } else {
-                                        numberToSegmentMapping[5] = example
-                                    }
-                                } else if (numberToSegmentMapping.contains(6)) {
-                                    val mask = numberToSegmentMapping.getOrDefault(6, setOf())
-                                    val res = example.filter { char -> !mask.contains(char) }
-                                    if (res.size == 1) {
-                                        numberToSegmentMapping[2] = example
-                                    } else {
-                                        numberToSegmentMapping[5] = example
-                                    }
+                                } else if (overlapsRightTop) {
+                                    numberToSegmentMapping[2] = example
+                                } else {
+                                    numberToSegmentMapping[5] = example
                                 }
                             }
                         }
                     }
                 }
 
-                return SevenSegmentDisplay(numberToSegmentMapping, words.map(CharArray::toSet))
+                return SevenSegmentDisplay(numberToSegmentMapping, words)
             }
         }
     }
 
     override fun partOne(): Any {
-        return input.map { (examples, words) ->
+        return input.sumOf { (examples, words) ->
             SevenSegmentDisplay.from(examples, words).values.filter {
                 listOf(
                     1,
@@ -97,11 +74,11 @@ class Day8 : Day(8) {
                     8
                 ).contains(it)
             }.size
-        }.sum()
+        }
     }
 
     override fun partTwo(): Any {
-        return input.map { (examples, words) -> SevenSegmentDisplay.from(examples, words).value }.sum()
+        return input.sumOf { (examples, words) -> SevenSegmentDisplay.from(examples, words).value }
     }
 
 }
